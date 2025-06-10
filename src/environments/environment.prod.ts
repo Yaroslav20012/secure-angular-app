01cdbb9a6dfc788663ed46ca1ade0d5a4b9da69a
Yaroslav <yaroslav_savelev_2017@mail.ru>
2025-06-10 17:32:13 +0300

    Удалён лишний api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://secure-api.onrender.com/api'  // ⚠️ Важно, чтобы был /api
+  url: 'https://secure-api.onrender.com'  // ⚠️ Важно, чтобы был /api
 };
